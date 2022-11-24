51863374325db84473fc812e42dd6f6996f2a055
pietro24 <canevarolopietro@gmail.com>
2022-11-24 15:33:21 +0100

    add talent base class orm

--- models/talent.py
+++ models/talent.py
@@ -1,7 +1,14 @@
 from sqlalchemy import Column, String, Integer, Boolean, DateTime, Float, JSON
+from datetime import datetime
+
+from sqlalchemy.orm import DeclarativeBase
 
 
-class Talent():
+class Base(DeclarativeBase):
+    pass
+
+
+class Talent(Base):
 
     id = Column(Integer(), primary_key=True)
     original_id = Column(String(255), unique=True, nullable=False)
@@ -25,3 +32,27 @@ class Talent():
     required_skills = Column(JSON)
     optional_skills = Column(JSON)
     is_unassigned = Column(Boolean())
+
+    def __init__(self, talent):
+        self.id = talent['id']
+        self.original_id = talent['originalId']
+        self.talent_id = talent['talentId']
+        self.talent_name = talent['talentName']
+        self.talent_grade = talent['talentGrade']
+        self.booking_grade = talent['bookingGrade']
+        self.operating_unit = talent['operatingUnit']
+        self.office_city = talent['officeCity']
+        self.office_postal_code = talent['officePostalCode']
+        self.job_manager_name = talent['jobManagerName']
+        self.job_manager_id = talent['jobManagerId']
+        self.total_hours = talent['totalHours']
+        self.start_date = datetime.strptime(
+            talent['startDate'], '%m/%d/%Y %I:%M %p')
+        self.end_date = datetime.strptime(
+            talent['endDate'], '%m/%d/%Y %I:%M %p')
+        self.client_name = talent['clientName']
+        self.client_id = talent['clientId']
+        self.industry = talent['industry']
+        self.required_skills = talent['requiredSkills']
+        self.optional_skills = talent['optionalSkills']
+        self.is_unassigned = talent['isUnassigned']
